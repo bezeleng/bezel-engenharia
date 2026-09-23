39b1911ab4782865ee825422ae7c7946c6b771d3
bezeleng <droliveir@hotmail.com>
2026-09-23 16:11:53 -0300

    Show highlights gallery before management section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,6 @@
 import { Hero } from "@/components/sections/Hero";
 import { ServicosGrid } from "@/components/sections/ServicosGrid";
+import { GaleriaDestaquesHome } from "@/components/sections/GaleriaDestaquesHome";
 import { SecaoGestao } from "@/components/sections/SecaoGestao";
 import { PortfolioCarousel } from "@/components/sections/PortfolioCarousel";
 import { DepoimentosDestaque } from "@/components/sections/DepoimentosDestaque";
@@ -10,6 +11,7 @@ export default function Home() {
     <>
       <Hero />
       <ServicosGrid />
+      <GaleriaDestaquesHome />
       <SecaoGestao />
       <PortfolioCarousel />
       <DepoimentosDestaque />
